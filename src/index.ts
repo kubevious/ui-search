bacdcc67b61f75aac3e672f66d33cec2125dc963
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-04-05 12:47:16 -0700

    exported filters.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { Search } from './Search'
 
+export { FilterSearchKinds } from './Filters/SearchKinds'
+export { FilterSearchLabel } from './Filters/SearchLabel'
+export { FilterSearchAnnotation } from './Filters/SearchAnnotation'
+export { FilterSearchErrors } from './Filters/SearchErrors'
+export { FilterSearchMarkers } from './Filters/SearchMarkers'
+export { FilterSearchWarnings } from './Filters/SearchWarnings'
+
 // types
 export {
     FilterItem,
