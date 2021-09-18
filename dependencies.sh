c4056fe4ad5e23ee69bef8d1c76d6dd0e163a4d5
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-09-17 18:49:58 -0700

    updated dependencies.

---
+++ dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPO_DEPENDENCIES=(
+    "the-lodash"
+    "the-promise"
+    "@kubevious/ui-framework"
+    "@kubevious/ui-middleware"
+    "@kubevious/ui-components"
+    "@kubevious/helpers"
+)
