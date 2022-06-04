654809f31784fcfa26295d79307fb47b38b7cc31
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-06-03 19:28:00 -0700

    updated dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import typescript from 'rollup-plugin-typescript2'
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 import pkg from './package.json'
 
 export default {
